a46fd65b2f37ea7e347282707a2a40f2ff2cca97
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-07-07 18:01:42 +0300

    Counter Example

--- app/src/main/java/com/moazsalem/jetpackplayground/MainActivity.kt
+++ app/src/main/java/com/moazsalem/jetpackplayground/MainActivity.kt
@@ -6,13 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.moazsalem.jetpackplayground.ui.theme.JetpackPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +29,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var counter by remember { mutableIntStateOf(0) }
             JetpackPlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column (modifier = Modifier.padding(innerPadding).fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally) {
                         Text(
-                            text = "Hello, world!",
+                            text = "$counter",
+                            fontSize = 48.sp,
                             modifier = Modifier.padding(innerPadding)
                         )
+                        Button(
+                            onClick = { counter++ },
+                        ) { Text("Increment", fontSize = 24.sp, modifier = Modifier.padding(4.dp)) }
+                        Spacer(Modifier.size(12.dp))
+                        Button(
+                            onClick = { counter--},
+                        ) { Text("Decrement", fontSize = 24.sp, modifier = Modifier.padding(4.dp)) }
                     }
                 }
             }
